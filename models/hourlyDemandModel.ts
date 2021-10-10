364dbd4f6688c496cdb21c305737981df6a6af0c
Hirochika Yuda <hyuda@outlook.jp>
2021-10-10 16:23:09 +0900

    Clarify License

--- models/hourlyDemandModel.ts
+++ models/hourlyDemandModel.ts
@@ -1,4 +1,22 @@
-import { DynamoDBClient, QueryCommand, QueryCommandInput, ScanCommand, ScanCommandInput } from "@aws-sdk/client-dynamodb"
+/*!
+    Japan Electricity Dashboard : GraphQL Server
+    Copyright (C) 2021 Hirochika Yuda, a.k.a. Kuropen.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { QueryCommand, QueryCommandInput, ScanCommand, ScanCommandInput } from "@aws-sdk/client-dynamodb"
 import { DynamoResult, HourlyDemand } from "../types"
 import DynamoClient from "./dynamoClient"
 
